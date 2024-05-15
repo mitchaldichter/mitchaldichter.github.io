ca354f92d261b21724db53fe9896a685da11e029
mitchaldichter <73209151+mitchaldichter@users.noreply.github.com>
2024-05-14 18:37:27 -0700

    canonical

--- create_sitemap.py
+++ create_sitemap.py
@@ -2,6 +2,7 @@ import os
 
 exclude_HTMLs = [
 "combining.html",
+"index.html",
 "partial_fraction_decomposition.html",
 "integration_by_parts.html",
 "integration_overview.html",
@@ -13,6 +14,7 @@ try:
 except OSError:
   pass
 sitemap_file = open("sitemap.txt", "a")
+sitemap_file.write("https://mitchaldichter.com/\n")
 for element in os.listdir():
   if ".html" in element and element not in exclude_HTMLs:
     sitemap_file.write("https://mitchaldichter.com/" + element + "\n")
